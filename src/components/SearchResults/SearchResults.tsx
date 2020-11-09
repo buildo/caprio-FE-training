d8128b08920b94ed9a7cda14f8ba737e6d49deb9
Vittorio Caprio <vittorio.caprio@buildo.io>
2020-11-10 00:30:13 +0100

    SearchResult component : prettify and convert as FC

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,30 +1,24 @@
-import React from "react"
-import View from "../View"
-import { pipe } from "fp-ts/lib/pipeable";
-import { FormattedMessage } from "react-intl";
+import React from 'react';
+import View from '../View';
+import { FormattedMessage } from 'react-intl';
 
-import "./searchresults.scss"
+import './searchresults.scss';
 
-const mockList: Array<string> = ["Item 1", "Item 2", "Item 3"]
+const mockList: string[] = ['Item 1', 'Item 2', 'Item 3'];
 
-class SearchResults extends React.Component {
-    render() {
-        return <View className="search-results" column hAlignContent={"center"}>
-            <h2 className="container-title" > <FormattedMessage id="Homepage.results.title" /> </h2>
-            {
-                pipe(
-                    mockList,
-                    (items) => (
-                        items.map(name => <View
-                            className="result-item"
-                            hAlignContent={"center"}
-                            width='100%'>  - {name}
-                        </View>)
-                    )
-                )
-            }
+const SearchResults: React.FC = () => {
+  return (
+    <View className="search-results" column hAlignContent={'center'}>
+      <h2 className="container-title">
+        <FormattedMessage id="Homepage.results.title" />
+      </h2>
+      {mockList.map(name => (
+        <View className="result-item" hAlignContent={'center'}>
+          - {name}
         </View>
-    }
-}
+      ))}
+    </View>
+  );
+};
 
-export default SearchResults
+export default SearchResults;
