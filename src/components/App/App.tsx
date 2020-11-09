0103c677c02221c10672e7cb3f93211d524e5ef6
Vittorio Caprio <vittorio.caprio@buildo.io>
2020-11-10 00:31:40 +0100

    Prettify App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,27 @@
-import * as React from "react";
-import View from "../View";
-import SearchBar from "../SearchBar";
-import SearchResults from "../SearchResults/SearchResults";
+import * as React from 'react';
+import View from '../View';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
 
-import { declareQueries } from "avenger/lib/react";
-import { currentView } from "../../queries";
-import { config } from "../../config";
+import { declareQueries } from 'avenger/lib/react';
+import { currentView } from '../../queries';
+import { config } from '../../config';
 
-import "./app.scss";
-import { Divider } from "buildo-react-components/lib";
+import './app.scss';
+import { Divider } from 'buildo-react-components/lib';
 
 const queries = declareQueries({ currentView });
 
 class App extends React.Component<typeof queries.Props> {
   render() {
     return (
-      <View column className="app layout" width='100%' height='100%' >
-        <View column hAlignContent={"center"}>
+      <View column className="app layout" height="100%">
+        <View column hAlignContent="center">
           <h1>{config.title}</h1>
           <SearchBar />
         </View>
         <Divider />
-        <View style={{ height: '60%' }}>          
+        <View style={{ height: '60%' }}>
           <SearchResults />
         </View>
       </View>
