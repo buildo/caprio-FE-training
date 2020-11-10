4dcae73a50d93d24a114336015e84c30a9026c42
Vittorio Caprio <vittorio.caprio@buildo.io>
2020-11-10 10:59:49 +0100

    Make SearchResults simply and pretty

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -6,19 +6,17 @@ import './searchresults.scss';
 
 const mockList: string[] = ['Item 1', 'Item 2', 'Item 3'];
 
-const SearchResults: React.FC = () => {
+export function SearchResults() {
   return (
-    <View className="search-results" column hAlignContent={'center'}>
+    <View className="search-results" column hAlignContent="center">
       <h2 className="container-title">
         <FormattedMessage id="Homepage.results.title" />
       </h2>
       {mockList.map(name => (
-        <View className="result-item" hAlignContent={'center'}>
+        <View className="result-item" hAlignContent="center">
           - {name}
         </View>
       ))}
     </View>
   );
-};
-
-export default SearchResults;
+}
