075229ae89c762bda38469a01bd5f3b53fa62329
Vittorio Caprio <vittorio.caprio@buildo.io>
2020-11-10 19:09:24 +0100

    Created search container component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,29 +1,19 @@
 import * as React from 'react';
 import View from '../View';
-import { SearchBar } from '../SearchBar/SearchBar';
-import { SearchResults } from '../SearchResults/SearchResults';
 
 import { declareQueries } from 'avenger/lib/react';
 import { currentView } from '../../queries';
-import { config } from '../../config';
 
 import './app.scss';
-import Divider from '../Diveder/Divider';
+import { SearchResturantContainer } from '../SearchResturantContainer/SearchResturantContainer';
 
 const queries = declareQueries({ currentView });
 
 class App extends React.Component<typeof queries.Props> {
   render() {
     return (
-      <View column className="app layout">
-        <View column hAlignContent="center" className="header" basis={300}>
-          <h1 className="title">{config.title}</h1>
-          <SearchBar />
-        </View>
-        <Divider />
-        <View grow>
-          <SearchResults />
-        </View>
+      <View column className="app layout">        
+        <SearchResturantContainer />
       </View>
     );
   }
