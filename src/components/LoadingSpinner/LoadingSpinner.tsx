c38d933abfdf912d4f02f1047909085aee94f17d
Vittorio Caprio <vittorio.caprio@buildo.io>
2020-11-13 15:16:57 +0100

    Use LoadingSpinner from buildo lib

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,18 +1,5 @@
-import * as React from 'react';
+import { LoadingSpinner } from 'buildo-react-components';
 
 import './loadingspinner.scss';
 
-export function LoadingSpinner() {
-  return (
-    <div className="lds-roller">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-  );
-}
+export default LoadingSpinner;
